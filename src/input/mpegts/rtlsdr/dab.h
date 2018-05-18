d19f2fe7c54b57629a3d20030b652ecdce524254
mwka <matthias@walliczek.de>
2018-05-18 19:12:06 +0200

    fix syntax

--- src/input/mpegts/rtlsdr/dab.h
+++ src/input/mpegts/rtlsdr/dab.h
@@ -2,7 +2,9 @@
 #define _DAB_H
 
 #include <stdint.h>
-#include "input_sdr.h"
+#include <fftw3.h>
+
+#include "sdr_fifo.h"
 
 /* A demapped transmission frame represents a transmission frame in
    the final state before the FIC-specific and MSC-specific decoding
@@ -63,6 +65,40 @@ struct ens_info_t {
   struct subchannel_info_t subchans[64];
 };
 
+#define DEFAULT_BUF_LENGTH (16 * 16384)
+#define GAIN_SETTLE_TIME 0
+
+struct sdr_state_t {
+	uint32_t frequency;
+	uint8_t input_buffer[DEFAULT_BUF_LENGTH];
+	int input_buffer_len;
+	uint8_t buffer[196608 * 2];
+	int32_t coarse_timeshift;
+	int32_t fine_timeshift;
+	int32_t coarse_freq_shift;
+	double fine_freq_shift;
+	CircularBuffer fifo;
+	int8_t real[196608];
+	int8_t imag[196608];
+	float filt[196608 - 2662];
+	fftw_complex * dab_frame;
+	fftw_complex * prs_ifft;
+	fftw_complex * prs_conj_ifft;
+	fftw_complex * prs_syms;
+	/* raw symbols */
+	fftw_complex symbols[76][2048];
+	/* symbols d-qpsk-ed */
+	fftw_complex * symbols_d;
+
+	int32_t startup_delay;
+	uint8_t force_timesync;
+
+	/* fault injection */
+	double p_e_prior_dep;
+	double p_e_prior_vitdec;
+	double p_e_after_vitdec;
+};
+
 struct dab_state_t
 {
   struct sdr_state_t device_state;
@@ -83,6 +119,8 @@ struct dab_state_t
   void (* eti_callback)(uint8_t *eti);
 };
 
+int sdr_demod(struct demapped_transmission_frame_t *tf, struct sdr_state_t *sdr);
+void sdr_init(struct sdr_state_t *sdr);
 void init_dab_state(struct dab_state_t **dab, void (* eti_callback)(uint8_t *eti));
 void dab_process_frame(struct dab_state_t *dab);
 
