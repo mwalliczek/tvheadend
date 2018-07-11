3f842bd701539997f319c2922f9ca7056e5336c1
mwka <matthias@walliczek.de>
2018-07-11 19:51:37 +0200

    use native complex

--- src/input/mpegts/rtlsdr/ofdmDecoder.h
+++ src/input/mpegts/rtlsdr/ofdmDecoder.h
@@ -2,7 +2,7 @@
 #define _OFDMDECODER_H
 
 void initOfdmDecoder(struct sdr_state_t *sdr);
-void processBlock_0(struct sdr_state_t *sdr, struct complex_t* v);
-void decodeBlock(struct sdr_state_t *sdr, struct complex_t* v, int32_t blkno);
+void processBlock_0(struct sdr_state_t *sdr, float _Complex* v);
+void decodeBlock(struct sdr_state_t *sdr, float _Complex* v, int32_t blkno);
 
 #endif
