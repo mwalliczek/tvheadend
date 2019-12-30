6e157627d22db56c2398bc111561c011d590f871
mwalliczek <matthias@walliczek.de>
2019-12-30 17:27:41 +0100

    cleanup unused code

--- src/input/dab/rtlsdr/fic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FIC_H
-#define _FIC_H
-
-void fib_parse(struct tf_info_t* info, uint8_t* fib);
-void fib_decode(struct tf_info_t *info, struct tf_fibs_t *fibs, int nfibs);
-void fic_decode(struct dab_state_t *dab, struct demapped_transmission_frame_t *tf);
-void dump_tf_info(struct tf_info_t* info);
-
-#endif
